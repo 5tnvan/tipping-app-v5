73b649837ca65aba7ed07b0925e9a123c7bcd0a4
vtasst <5tnvan@gmail.com>
2024-04-12 14:13:01 +0200

    SPRINT-02 update deployed contract

--- packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
+++ packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
@@ -38,6 +38,7 @@ export const ScaffoldEthAppWithProviders = ({ children }: { children: React.Reac
   //const subgraphUriEthereumSepolia = "https://api.studio.thegraph.com/query/68297/wildpay-sepolia-v4/0.0.1";
   //const subgraphUriEthereumMainnet = "https://api.studio.thegraph.com/query/68297/wildpay-eth-mainnet/0.0.1";
   //const subgraphUriBaseSepolia = "https://api.studio.thegraph.com/query/68297/wildpay-base-sepolia/0.0.1";
+  //const subgraphUriBaseMainnet = "https://api.studio.thegraph.com/query/68297/wildpay-base-mainnet/0.0.1";
 
   const client = new ApolloClient({
     uri: undefined,
