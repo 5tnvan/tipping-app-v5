6271c35a43478d61bc6cdd605abe06f51eccc463
vtasst <5tnvan@gmail.com>
2024-04-14 16:44:16 +0200

    SPRINT-03 Feature: detect facelandmark

--- packages/nextjs/app/private/page.tsx
+++ packages/nextjs/app/private/page.tsx
@@ -1,8 +1,13 @@
 "use client";
 
 import { NextPage } from "next";
+import AvatarCreator from "~~/components/app/avatar/page";
 
 const PrivatePage: NextPage = () => {
-  return <div className="text-white">private, test</div>;
+  return (
+    <div className="bg-white text-black">
+      <AvatarCreator />
+    </div>
+  );
 };
 export default PrivatePage;
