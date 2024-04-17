c09e9e6ba32473022f57ea15c0b169ef604ee002
vtasst <5tnvan@gmail.com>
2024-04-18 01:13:46 +0200

    SPRINT-03 Css fix

--- packages/nextjs/components/app/authentication/Avatar.tsx
+++ packages/nextjs/components/app/authentication/Avatar.tsx
@@ -17,7 +17,7 @@ export const Avatar = ({ profile, width, ring }: Props) => {
 
   return (
     <>
-      <div className={`rounded-full ${ring ? `avatar1 w-${width + 2} h-${width + 2} moving-gradient` : ""}`}>
+      <div className={`flex items-center justify-center rounded-full ${ring ? `w-${width + 2} h-${width + 2} moving-gradient` : ""}`}>
         {profile.avatar_url && (
           <div className={`rounded-full w-${width} h-${width} border-2 border-slate-200`}>
             <Image className="rounded-full" alt="img" src={profile.avatar_url} width={100} height={100} />
