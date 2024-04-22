043485791d4efd4432c10ce61363b257493bb7c8
vtasst <5tnvan@gmail.com>
2024-04-22 12:24:23 +0200

    SPRINT-03 private page build

--- packages/nextjs/app/private/page.tsx
+++ packages/nextjs/app/private/page.tsx
@@ -1,23 +1,22 @@
 "use client";
 
-import { useContext, useEffect, useState } from "react";
-import { AppContext } from "../context";
 import { NextPage } from "next";
 import AvatarCreator from "~~/components/app/avatar/page";
-import { fetchFollowersWithRange } from "~~/utils/app/fetch/fetchFollowers";
 
 const PrivatePage: NextPage = () => {
-  const { profile } = useContext(AppContext);
-  const [res, setRes] = useState<any>();
-  useEffect(() => {
-    async function fetchData() {
-      const result = await fetchFollowersWithRange("30e68e4b-7e19-43be-b890-0592e3e1ec38");
-      setRes(result);
-    }
-    fetchData();
-  }, [profile]);
-
-  console.log("res", res);
-  return <div className="bg-white text-black">{/* <AvatarCreator /> */}</div>;
+  // const { profile } = useContext(AppContext);
+  // const [res, setRes] = useState<any>();
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const result = await fetchFollowersWithRange("30e68e4b-7e19-43be-b890-0592e3e1ec38");
+  //     setRes(result);
+  //   }
+  //   fetchData();
+  // }, [profile]);
+  return (
+    <div className="bg-white text-black">
+      <AvatarCreator />
+    </div>
+  );
 };
 export default PrivatePage;
